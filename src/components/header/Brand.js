729e56cc5db214c436d0ce7b6d9d321e4973a4f0
Christian Rodriguez <christ.rod@gmail.com>
2023-01-24 03:29:24 -0500

    Creacion Brand

---
+++ src/components/header/Brand.js
@@ -0,0 +1,10 @@
+import brand from "../../img/sulpayki.jpg";
+
+const Brand = () => {
+    return (
+        <img src={brand} alt="Tienda Sulpayki" title= "Tienda Sulpayki"></img>
+    )
+}
+
+export default Brand;
+
---
+++ src/components/header/NavBar.js
@@ -0,0 +1,21 @@
+import Brand from "./Brand";
+
+const Navbar = () => {
+    return(
+        <header>
+            <div className="containerBrand">
+                <Brand />
+            </div>
+
+            <nav className="containerItemList">
+
+            </nav>
+
+            <div className="containerCart">
+
+            </div>
+        </header>
+    )
+}
+
+export default Navbar;
